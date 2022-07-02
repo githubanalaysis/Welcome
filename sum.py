9307e6a84da1bed8e701d7d92e841c950fbe8482
SrikanthGit.test <analysisdata298@gmail.com>
2022-07-02 16:03:49 +0530

    print statement added

--- sum.py
+++ sum.py
@@ -6,5 +6,6 @@ print("Added new statement")
 print("Added new statement1")
 <<<<<<< HEAD
 print("Added end statement")
+print("One more end statement")
 =======
 >>>>>>> 9f830299b7021038b0bce5e69ff04059c1eb4821
